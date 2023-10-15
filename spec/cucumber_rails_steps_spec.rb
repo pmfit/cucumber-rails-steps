591f4a87543f9d323d02cb72d9b1b5a9f0e716c1
nolan <nolan@ncphi.com>
2023-10-15 11:02:34 -0300

    confirm "edit user" path is supported

--- spec/cucumber_rails_steps_spec.rb
+++ spec/cucumber_rails_steps_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe CucumberRailsSteps do
         ['users', :users_path],
         ['user', :user_path],
         ['new user', :new_user_path],
+        ['edit user', :edit_user_path],
       ].each  do |name, expected|
         it "returns #{expected} for #{name}" do
           expect(resolver.resolve_method(name)).to eq(expected)
